f9759de9c99f31a1f4785503af14c27322615b65
Farouk-Aiman <55766695+Farouk-Aiman@users.noreply.github.com>
2019-09-29 07:04:33 +0200

    Initial commit

---
+++ js/plugins.js
@@ -0,0 +1,41 @@
+/*global $*/
+(function () {
+	'use strict';
+	$('.carousel').carousel({
+		interval: 2000000
+	});
+	$('body').css({
+		"padding-top": $('.navbar').height() + 16
+	});
+	$(".Portfolio .work").mouseenter(function () {
+		$(this).children(".Portfolio .work .over").hide(500);
+		$(this).children(".Portfolio .work .overlay").show(500);
+	});
+	$(".Portfolio .work").mouseleave(function () {
+		$(this).children(".Portfolio .work .over").show(500);
+		$(this).children(".Portfolio .work .overlay").fadeOut(500);
+	});
+	
+	$(".navbar .collapse .navbar-nav li").click().addClass("active").siblings().removeClass("active");
+	
+	$(".itemTwo").click(function () {
+		$("html , body").animate({
+			scrollTop: $("#features").offset().top - $('.navbar').height() - 16
+		}, 500);
+	});
+	$(".itemThree").click(function () {
+		$("html , body").animate({
+			scrollTop: $("#pricing").offset().top - $('.navbar').height() - 16
+		}, 500);
+	});
+	$(".itemFour").click(function () {
+		$("html , body").animate({
+			scrollTop: $("#review").offset().top - $('.navbar').height() - 16
+		}, 500);
+	});
+	$(".itemFive").click(function () {
+		$("html , body").animate({
+			scrollTop: $("footer").offset().top - $('.navbar').height() - 16
+		}, 700);
+	});
+}());
